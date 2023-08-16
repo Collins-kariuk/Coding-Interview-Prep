d5761ee59150775a92eb615847c8b20553748864
Collins Kariuki <cmka2020@mymail.pomona.edu>
2023-08-15 19:15:49 -0700

    Solve "Invert Binary Tree"

--- Blind 75.py
+++ Blind 75.py
@@ -29,3 +29,30 @@ def removeNthFromEnd(head, n):
     # Return dummy.next
     return dummy.next
     
+
+### TREES ###
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# --------- 2. Invert Binary Tree - Leetcode 19 - Medium ------------
+def invertTree(root):
+    # check whether the root contains value and if not return none
+    if root == None:
+        return None
+
+    # swap the left and right children
+    # you have to store the left child in a variable first
+    # to not lose it
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    # recursively call the method on the right and left children
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
