514518b852f0a0360263b7146944b3608d22a8e3
Collins Kariuki <cmka2020@mymail.pomona.edu>
2023-08-22 09:11:19 -0700

    An implementation of a trie in Python

--- Special Topics.py
+++ Special Topics.py
@@ -29,3 +29,61 @@ if __name__ == '__main__':
     graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
     print("Following is Breadth First Traversal: ")
     bfs(graph, 0)
+    
+
+### ------ Tries in Python --------- ###
+class Node:
+	def __init__(self):
+		self.key = None
+		self.value = None
+		self.children = {}
+	
+class Trie:
+	def __init__(self):
+		self.root = Node()
+	
+	def insert(self, word, value):
+		currentWord = word
+		currentNode = self.root
+
+		while len(currentWord) > 0:
+			if currentWord[0] in currentNode.children:
+				currentNode = currentNode.children[currentWord[0]]
+				currentWord = currentWord[1:]
+			else:
+				newNode = Node()
+				newNode.key = currentWord[0]
+				if len(currentWord) == 1:
+					newNode.value = value
+				currentNode.children[currentWord[0]] = newNode
+				currentNode = newNode
+				currentWord = currentWord[1:]
+		
+	def lookup(self, word):
+		currentWord = word
+		currentNode = self.root
+		while len(currentWord) > 0:
+			if currentWord[0] in currentNode.children:
+				currentNode = currentNode.children[currentWord[0]]
+				currentWord = currentWord[1:]
+			else:
+				return "Not in trie"
+		
+		if currentNode.value == None:
+			return "None"
+		return currentNode.value
+	
+	def printAllNodes(self):
+		nodes = [self.root]
+		while len(nodes) > 0:
+			for letter in nodes[0].children:
+				nodes.append(nodes[0].children[letter])
+            # print(nodes.pop(0).key)
+
+def makeTrie(words):
+    trie = Trie()
+    for word, value in words.items():
+        trie.insert(word, value)
+    return trie
+
+		
