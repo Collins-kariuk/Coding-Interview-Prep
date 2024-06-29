8a31a56e481120479bb55103072fc2033e190da1
Collins Kariuki <cmka2020@mymail.pomona.edu>
2024-06-29 11:53:56 -0700

    Solve Contains Duplicate in JS

---
+++ Neetcode/JavaScript/Blind 75.js
@@ -0,0 +1,22 @@
+/// ARRAYS & HASHING ///
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate(nums) {
+    // Initialize an object that will store the occurrences of elements in nums
+    let dictList = {};
+    for (let num of nums) {
+        // If the number already exists in the object, nums contains a duplicate
+        if (dictList.hasOwnProperty(num)) {
+            return true;
+        }
+        // If the number does not already exist in the object, add it
+        else {
+            dictList[num] = 1;
+        }
+    }
+    // We've gone through all numbers in nums and have not found any duplicates
+    return false;
+}
